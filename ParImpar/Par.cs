35dee9a35787e367e5e9ca2879013035e02e0174
BD1 <brunodesa288@gmail.com>
2019-11-18 16:14:30 +0000

    Adicionar arquivos de projeto.

---
+++ ParImpar/Par.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParImpar
+{
+    class Par
+    {
+        public double valor;
+        public string recebe
+        {
+            set
+            {
+                valor = Convert.ToInt32(value);
+            }
+
+        }
+        public string envia
+        {
+            get
+            {
+                return "";
+            }
+
+        }
+
+    }
+}
